2ebf68efc23b126f4edb75dca4c8027fcf3e1c40
Wendy Harris <Wendy.Harris@ibm.com>
2022-03-30 15:58:27 -0400

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+docker build -t wendyharris/multi-client:latest -t wendyharris/multiclient:$SHA -f ./client/Dockerfile ./client
+docker build -t wendyharris/multi-server:latest -t wendyharris/multiserver:$SHA  -f ./server/Dockerfile ./server
+docker build -t wendyharris/multi-worker:latest -t wendyharris/multiworker:$SHA  -f ./worker/Dockerfile ./worker
+docker push wendyharris/multi-client:latest
+docker push wendyharris/multi-server:latest
+docker push wendyharris/multi-worker:latest
+docker push wendyharris/multi-client:$SHA
+docker push wendyharris/multi-server:$SHA
+docker push wendyharris/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=wendyharris/multi-server:$SHA
+kubectl set image deployments/client-deployment client=wendyharris/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=wendyharris/multi-worker:$SHA
+
